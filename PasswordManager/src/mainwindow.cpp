a3f3062701a9a7d9002b08ce387f620225f59798
Jan S <jato@fedora.fritz.box>
2025-11-20 19:51:56 +0100

    general UI improvements

--- PasswordManager/src/mainwindow.cpp
+++ PasswordManager/src/mainwindow.cpp
@@ -13,3 +13,65 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+//Page navigation (stacked Widget)
+
+void MainWindow::on_sidebar_lock_clicked()
+{
+    ui->sidebar_lock->setChecked(true);
+
+    ui->stackedWidget->setCurrentIndex(0);
+
+    ui->sidebar_passwords->setChecked(false);
+    ui->sidebar_healthcheck->setChecked(false);
+    ui->sidebar_passwordgenerator->setChecked(false);
+    ui->sidebar_settings->setChecked(false);
+}
+
+void MainWindow::on_sidebar_passwords_clicked()
+{
+    ui->sidebar_passwords->setChecked(true);
+
+    ui->stackedWidget->setCurrentIndex(1);
+
+    ui->sidebar_lock->setChecked(false);
+    ui->sidebar_healthcheck->setChecked(false);
+    ui->sidebar_passwordgenerator->setChecked(false);
+    ui->sidebar_settings->setChecked(false);
+}
+
+void MainWindow::on_sidebar_healthcheck_clicked()
+{
+    ui->sidebar_healthcheck->setChecked(true);
+
+    ui->stackedWidget->setCurrentIndex(2);
+
+    ui->sidebar_passwords->setChecked(false);
+    ui->sidebar_lock->setChecked(false);
+    ui->sidebar_passwordgenerator->setChecked(false);
+    ui->sidebar_settings->setChecked(false);
+}
+
+void MainWindow::on_sidebar_passwordgenerator_clicked()
+{
+    ui->sidebar_passwordgenerator->setChecked(true);
+
+    ui->stackedWidget->setCurrentIndex(3);
+
+    ui->sidebar_passwords->setChecked(false);
+    ui->sidebar_healthcheck->setChecked(false);
+    ui->sidebar_lock->setChecked(false);
+    ui->sidebar_settings->setChecked(false);
+}
+
+void MainWindow::on_sidebar_settings_clicked()
+{
+    ui->sidebar_settings->setChecked(true);
+
+    ui->stackedWidget->setCurrentIndex(4);
+
+    ui->sidebar_passwords->setChecked(false);
+    ui->sidebar_healthcheck->setChecked(false);
+    ui->sidebar_passwordgenerator->setChecked(false);
+    ui->sidebar_lock->setChecked(false);
+}
+
