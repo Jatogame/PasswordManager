dd65f298570ec557118309216a49b77a4434c8a9
Jan Schellenberg <janschell2001@gmail.com>
2025-11-21 13:29:31 +0100

    Lockscreen, Loginscreen

--- PasswordManager/src/main.cpp
+++ PasswordManager/src/main.cpp
@@ -7,6 +7,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     //Open Main Window
     MainWindow w;
+    //Open the Main Window locked
+    w.open_locked();
+
     w.show();
     return a.exec();
 }
