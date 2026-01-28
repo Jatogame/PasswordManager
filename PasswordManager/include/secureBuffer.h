efd8669c2705db29378df429ffa427b1ff684c7f
Jan S <jato@fedora.fritz.box>
2026-01-28 17:05:24 +0100

    deleted unnecessary files

--- PasswordManager/include/secureBuffer.h
+++
@@ -1,6 +0,0 @@
-#ifndef SECUREBUFFER_H
-#define SECUREBUFFER_H
-
-// safety against Dumps, Swap, Use-after-free
-
-#endif // SECUREBUFFER_H
--- PasswordManager/include/vault.h
+++
@@ -1,6 +0,0 @@
-#ifndef VAULT_H
-#define VAULT_H
-
-// handling of the temp decrypted data
-
-#endif // VAULT_H
