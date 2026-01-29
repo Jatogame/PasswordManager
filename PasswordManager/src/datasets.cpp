e84cf9e25e3aed042dc22de05e045b28ac353142
Jan S <jato@fedora.fritz.box>
2026-01-29 03:12:06 +0100

    derive key function edited

---
+++ PasswordManager/src/datasets.cpp
@@ -0,0 +1,4 @@
+#include "dbHeader.h"
+//make access to structs global
+RunTimeData runTime;
+DbHeader metaData;
--- PasswordManager/src/derivePassword.cpp
+++ PasswordManager/src/derivePassword.cpp
@@ -2,11 +2,30 @@
 #include <QFileDialog>
 #include <QSqlDatabase>
 #include "dbHeader.h"
+#include <sodium.h>
 
 void derivePassword(QByteArray masterPassword){
-    RunTimeData runTime;
+
     //use argon2id to derive password
-    QByteArray derPassword;
+    QByteArray derPassword (32, 0); //32 Bytes for ChaCha20
+
+    //calculate salt
+    QByteArray salt(crypto_pwhash_SALTBYTES, 0);
+    randombytes_buf(salt.data(), salt.size());
+    metaData.salt = salt;   //save salt in the struct DbHeader
+
+    //calculate Argon2id
+    int result = crypto_pwhash(
+        reinterpret_cast<unsigned char*>(derPassword.data()),
+        derPassword.size(),
+        masterPassword.constData(),
+        masterPassword.size(),
+        reinterpret_cast<const unsigned char*>(salt.constData()),
+        metaData.iterations,
+        metaData.memoryCost,
+        crypto_pwhash_ALG_ARGON2ID13
+        );
+
     //save derived password
     runTime.derPass = derPassword;
 };
